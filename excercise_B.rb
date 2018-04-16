4da811afca19f45f00df9358d33403176c1a7d05
Colin Tindle <ampersand@underscore.space>
2018-04-16 16:53:20 +0100

    removed set/get methods

--- excercise_B.rb
+++ excercise_B.rb
@@ -16,28 +16,11 @@ class Team
     :coach
   )
 
-  attr_writer(:coach)
+  attr_accessor(:coach)
   
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
   end
-
-  # def get_team_name
-  #   return @team_name
-  # end
-  #
-  # def get_team_players
-  #   return @players
-  # end
-  #
-  # def get_team_coach
-  #   return @coach
-  # end
-  #
-  # def set_team_coach(new_coach)
-  #   @coach = new_coach
-  # end
-
 end
