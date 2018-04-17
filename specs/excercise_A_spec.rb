00280ee5eb1a82380ba2408383fc40ad10ff66a9
Colin Tindle <ampersand@underscore.space>
2018-04-17 09:42:43 +0100

    moved object setup to minitest setup method for both

--- specs/excercise_A_spec.rb
+++ specs/excercise_A_spec.rb
@@ -3,37 +3,35 @@ require('minitest/rg')
 require_relative('../excercise_A')
 
 class StudentTest < MiniTest::Test
+  def setup
+    @student = Student.new('Colin', 21)
+  end
+
   def test_get_student_name
-    student1 = Student.new('Colin', 21)
-    assert_equal('Colin', student1.get_name())
+    assert_equal('Colin', @student.get_name())
   end
 
   def test_get_student_cohort
-    student1 = Student.new('Colin', 21)
-    assert_equal(21, student1.get_cohort())
+    assert_equal(21, @student.get_cohort())
   end
 
   def test_set_student_name
-    student1 = Student.new('Colin', 21)
-    student1.set_name("Luis")
-    assert_equal("Luis", student1.get_name())
+    @student.set_name("Luis")
+    assert_equal("Luis", @student.get_name())
   end
 
   def test_set_student_cohort
-    student1 = Student.new('Colin', 21)
-    student1.set_cohort(20)
-    assert_equal(20, student1.get_cohort())
+    @student.set_cohort(20)
+    assert_equal(20, @student.get_cohort())
   end
 
   def test_student_talk
-    student1 = Student.new('Colin', 21)
-    result = student1.talk()
+    result = @student.talk()
     assert_equal('I can talk!', result)
   end
 
   def test_student_fav_language
-    student1 = Student.new('Colin', 21)
-    result = student1.say_favourite_language("Ruby")
+    result = @student.say_favourite_language("Ruby")
     assert_equal('I love Ruby', result)
   end
 end
--- specs/excercise_B_spec.rb
+++ specs/excercise_B_spec.rb
@@ -3,67 +3,61 @@ require('minitest/rg')
 require_relative('../excercise_B')
 
 class SportTeamTest < MiniTest::Test
+  def setup
+    @team = Team.new('Partick', ['Jim', 'Bob'], "Richard")
+  end
+  
 
   def test_get_team_name
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_name = team1.team_name()
-    assert_equal("Hearts", team_name)
+    team_name = @team.team_name()
+    assert_equal("Partick", team_name)
   end
 
   def test_get_team_players
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_players = team1.players()
-    assert_equal(["luis", "colin"], team_players)
+    team_players = @team.players()
+    assert_equal(["Jim", "Bob"], team_players)
   end
 
   def test_get_team_coach
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team_coach = team1.coach()
-    assert_equal("Sandy", team_coach)
+    team_coach = @team.coach()
+    assert_equal("Richard", team_coach)
   end
 
   def test_set_team_coach
-    team1 = Team.new("Hearts", ["luis", "colin"], "Sandy")
-    team1.coach = 'John'
-    team_coach = team1.coach()
+    @team.coach = 'John'
+    team_coach = @team.coach()
     assert_equal("John", team_coach)
   end
 
   def test_add_new_player
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    team1.add_player('Jeff')
-    assert_equal(['Jim', 'Bob', 'Jeff'], team1.players)
+    @team.add_player('Jeff')
+    assert_equal(['Jim', 'Bob', 'Jeff'], @team.players)
   end
 
   def test_is_player__true
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    is_player = team1.is_player('Jim')
+    is_player = @team.is_player('Jim')
     assert_equal(true, is_player)
   end
 
   def test_is_player__false
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    is_player = team1.is_player('Jeff')
+    is_player = @team.is_player('Jeff')
     assert_equal(false, is_player)
   end
 
   def test_points_is_zero
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    points = team1.points
+    points = @team.points
     assert_equal(0, points)
   end
 
   def test_game_won__win
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    team1.game_won(true)
-    points = team1.points
+    @team.game_won(true)
+    points = @team.points
     assert_equal(1, points)
   end
 
   def test_game_won__lose
-    team1 = Team.new('Partick', ['Jim', 'Bob'], "Richard")
-    team1.game_won(false)
-    points = team1.points
+    @team.game_won(false)
+    points = @team.points
     assert_equal(0, points)
   end
 
